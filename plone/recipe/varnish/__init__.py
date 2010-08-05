8ed54544a887c7cecd30a27be252ba349355f511
Hanno Schlichting <hanno@hannosch.eu>
2010-08-05 09:18:10 +0000

    Go straight to 2.1.3
    svn path=/buildout/plone.recipe.varnish/trunk/; revision=122910

--- plone/recipe/varnish/__init__.py
+++ plone/recipe/varnish/__init__.py
@@ -33,7 +33,7 @@ class ConfigureRecipe:
                 buildout["buildout"]["parts-directory"], self.name)
 
         # Expose the download url of a known-good Varnish release
-        url = "http://downloads.sourceforge.net/varnish/varnish-2.1.2.tar.gz"
+        url = "http://downloads.sourceforge.net/varnish/varnish-2.1.3.tar.gz"
         self.options.setdefault("download-url", url)
 
         # Set some default options
