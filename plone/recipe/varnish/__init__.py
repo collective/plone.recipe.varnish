30494095aade9a1aa36f0efc967f4160a0543eda
Cleber J Santos <cleber@cleberjsantos.com.br>
2014-10-17 14:37:30 -0300

    Default Varnish version changes for 3.0.6

--- plone/recipe/varnish/__init__.py
+++ plone/recipe/varnish/__init__.py
@@ -109,7 +109,7 @@ class ConfigureRecipe:
         # Set some default options
         self.options.setdefault('varnish_version', '3')
         if self.options['varnish_version'] == '3':
-            url = 'http://repo.varnish-cache.org/source/varnish-3.0.5.tar.gz'
+            url = 'http://repo.varnish-cache.org/source/varnish-3.0.6.tar.gz'
         else:
             url = 'http://repo.varnish-cache.org/source/varnish-2.1.5.tar.gz'
         self.options.setdefault('download-url', url)
