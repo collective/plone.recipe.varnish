5e67e48aca737c1d86faacb0c03790643ab536b4
Maurits van Rees <maurits@vanrees.org>
2017-12-15 19:17:16 +0100

    Use commands.getoutput instead of a custom exec_system in the tests.

--- plone/recipe/varnish/tests/test_recipe.py
+++ plone/recipe/varnish/tests/test_recipe.py
@@ -5,10 +5,9 @@ from zc.buildout.testing import buildoutTearDown
 from zc.buildout.testing import install
 from zc.buildout.testing import install_develop
 
+import commands
 import doctest
 import shutil
-import subprocess
-import sys
 import unittest
 
 
@@ -16,33 +15,10 @@ FLAGS = \
     doctest.ELLIPSIS | \
     doctest.NORMALIZE_WHITESPACE | \
     doctest.REPORT_ONLY_FIRST_FAILURE
-
-# FIXME - check for other platforms
-MUST_CLOSE_FDS = not sys.platform.startswith('win')
-
-
-def exec_system(command, input='', with_exit_code=False):
-    p = subprocess.Popen(
-        command,
-        shell=True,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        close_fds=MUST_CLOSE_FDS
-    )
-    i, o, e = (p.stdin, p.stdout, p.stderr)
-    if input:
-        i.write(input.encode('utf8'))
-    i.close()
-    result = o.read() + e.read()
-    o.close()
-    e.close()
-    output = result.decode('utf-8')
-    if with_exit_code:
-        # Use the with_exit_code=True parameter when you want to test the exit
-        # code of the command you're running.
-        output += 'EXIT CODE: %s' % p.wait()
-    return output
+# We used to have a custom exec_system from zc.buildout,
+# but that one fails to give output back with varnish 4.1.
+# So we fall back to a simpler version.
+exec_system = commands.getoutput
 
 
 def setUp(test):
