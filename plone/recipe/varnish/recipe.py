303c287fcded17627809d7deb07b6dc6c9ef01f0
Fred van Dijk <fredvd@gmail.com>
2018-12-28 16:47:45 +0100

    fix code-analysis, add W504 & W605 to flake8-ignore, move cfg to base.

--- plone/recipe/varnish/recipe.py
+++ plone/recipe/varnish/recipe.py
@@ -329,7 +329,7 @@ class ConfigureRecipe(BaseRecipe):
         config['custom'] = {}
         for name in ('vcl_recv', 'vcl_hit', 'vcl_miss',
                      'vcl_backend_fetch', 'vcl_purge', 'vcl_deliver',
-                     'vcl_pipe', 'vcl_backend_response','vcl_hash'):
+                     'vcl_pipe', 'vcl_backend_response', 'vcl_hash'):
             config['custom'][name] = self.options.get(name, '')
 
         config['backends'] = self._process_backends()
